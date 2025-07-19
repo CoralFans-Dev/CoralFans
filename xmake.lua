d0e4636a3e1d14dcafdd4f4d20ea1753342301be
kuangby <3091240752@qq.com>
2025-07-19 17:37:10 +0800

    update to 2.3.1

--- xmake.lua
+++ xmake.lua
@@ -15,7 +15,7 @@ add_repositories("coralfansdev-repo https://github.com/CoralFans-Dev/xmake-repo.
 
 add_requires(
     "levilamina 1.2.0", {configs = {target_type = "server"}},
-    "levibuildscript",
+    "levibuildscript 0.4.0",
     "bsci main"
 )
 
